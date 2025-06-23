55a4b9e15e50d1f6b8b86247c77e9db9bf6812c3
sijalneupane <sijalneupane5@gmail.com>
2025-06-23 13:43:41 +0545

    Feat: users2 entity registered in auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controller/auth.controller';
+import { Users2 } from './entity/users2.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Users2])], // Add your entities here if needed
   providers: [AuthService],
   controllers: [AuthController]
 })
