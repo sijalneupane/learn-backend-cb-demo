0543919269d3a3f29a8c079eaa642e23ccee2b3b
sijalneupane <sijalneupane5@gmail.com>
2025-06-23 16:40:03 +0545

    Feat:Jwt module and passport module registered as imports in auth module.ts

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,10 +3,24 @@ import { AuthService } from './services/auth.service';
 import { AuthController } from './controller/auth.controller';
 import { Users2 } from './entity/users2.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users2])], // Add your entities here if needed
+  imports: [TypeOrmModule.forFeature([Users2]),
+  PassportModule,
+ JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService],
+    }),
+], // Add your entities here if needed
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
