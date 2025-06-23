7a042bca77b19821202915e9c31e864a694f8a37
sijalneupane <sijalneupane5@gmail.com>
2025-06-23 13:40:31 +0545

    Feat: validation pipe ued in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({whitelist: true}));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
