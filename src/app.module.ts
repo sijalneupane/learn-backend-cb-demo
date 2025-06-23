8f774403edc38da5192db845c486afcc76fc27bd
sijalneupane <sijalneupane5@gmail.com>
2025-06-23 13:41:46 +0545

    Feat: Config service and TypeORM configuration added in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import ormConfig from './core/config/migrations/orm.config';
+import { AppService } from './core/app/app.service';
+import { AppController } from './core/app/app.controller';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-    imports: [],
-    controllers: [],
-    providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+      load: [ormConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return configService.getOrThrow<TypeOrmModule>('orm.config');
+      },
+    }),
+
+    AuthModule,
+
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
