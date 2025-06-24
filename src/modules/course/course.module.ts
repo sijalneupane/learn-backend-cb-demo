75fd1477ca4504ee266afd05d6f6d0ecbe16d8cd
sijalneupane <sijalneupane5@gmail.com>
2025-06-24 15:29:26 +0545

    Feat: Course controller created

--- src/modules/course/course.module.ts
+++ src/modules/course/course.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { CourseController } from './controller/course.controller';
 
-@Module({})
+@Module({
+  controllers: [CourseController]
+})
 export class CourseModule {}
