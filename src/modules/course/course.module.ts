4ad14f2893f8cca3e755e7796d7f3ede94f5c57c
sijalneupane <sijalneupane5@gmail.com>
2025-06-24 16:31:58 +0545

    Feat: AUthmodule imported in coursemodule

--- src/modules/course/course.module.ts
+++ src/modules/course/course.module.ts
@@ -4,9 +4,10 @@ import { CourseService } from './service/course.service';
 import { Type } from 'class-transformer';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from './entity/course.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Course])], // Add your entities here if needed
+  imports: [TypeOrmModule.forFeature([Course]), AuthModule], // Add your entities here if needed
   controllers: [CourseController],
   providers: [CourseService],
 })
