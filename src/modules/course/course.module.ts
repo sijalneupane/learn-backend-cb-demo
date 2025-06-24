1f16a6d045bfef92aeead174486fed329270353a
sijalneupane <sijalneupane5@gmail.com>
2025-06-24 15:52:28 +0545

    Feat: Imported course entity in course module

--- src/modules/course/course.module.ts
+++ src/modules/course/course.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CourseController } from './controller/course.controller';
 import { CourseService } from './service/course.service';
+import { Type } from 'class-transformer';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Course } from './entity/course.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Course])], // Add your entities here if needed
   controllers: [CourseController],
   providers: [CourseService],
 })
