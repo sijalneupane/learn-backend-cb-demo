4be93d269906f0d35bd186e2baec5f8bed18ae12
sijalneupane <sijalneupane5@gmail.com>
2025-06-24 22:04:41 +0545

    Feat: Implement course management features in CourseController and CourseService using relationships  between users and courses

--- src/modules/course/controller/course.controller.ts
+++ src/modules/course/controller/course.controller.ts
@@ -1,4 +1,64 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { CourseService } from '../service/course.service';
+import { JWTAuthGuard } from 'src/core/guards/jwt-authenticate.guard';
+import { RolesGuard } from 'src/core/guards/role.guard';
+import { Roles } from 'src/core/decorators/roles.decorator';
+import { Role } from 'src/modules/auth/enum/role.enum';
+import { CreateCourseDto } from '../dto/create-course.dto';
 
+@UseGuards(JWTAuthGuard, RolesGuard)
 @Controller('course')
-export class CourseController {}
+export class CourseController {
+  constructor(private readonly courseService: CourseService) {}
+
+  @Post('create')
+  @Roles(Role.ADMIN)
+  async createCourse(@Body() createCourseDto: CreateCourseDto) {
+    return this.courseService.createCourse(createCourseDto);
+  }
+
+  @Get('view/:courseId')
+  @Roles(Role.USER, Role.ADMIN)
+  async getCourseById(@Param('courseId') courseId: number) {
+    return this.courseService.findById(courseId);
+  }
+
+  // Remove guards for this route only
+  @Get('allcourses')
+  @Roles(Role.USER, Role.ADMIN)
+  @UseGuards() // This disables guards for this route
+  async viewAllCourses() {
+    return this.courseService.findAll();
+  }
+
+  @Post('delete/:courseId')
+  @Roles(Role.ADMIN)
+  async deleteCourseById(@Param('courseId') courseId: number) {
+    return this.courseService.deleteCourseById(courseId);
+  }
+
+  @Post('join/:courseId')
+  @Roles(Role.USER)
+  async joinCourse(@Req() req, @Param('courseId') courseId: number) {
+    return this.courseService.joinCourseByUser(req.user.sub, courseId);
+  }
+  @Get('mycourses')
+  @Roles(Role.USER)
+  async getMyCourses(@Req() req) {
+    return this.courseService.fetchCourseOfUser(req.user.sub);
+  }
+
+  @Get('total-learners/:courseId')
+  @Roles(Role.USER, Role.ADMIN)
+  async getTotalLearnersInCourse(@Param('courseId') courseId: number) {
+    return this.courseService.findTotalLearnersInACourse(courseId);
+  }
+}
--- src/modules/course/service/course.service.ts
+++ src/modules/course/service/course.service.ts
@@ -1,4 +1,114 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Course } from '../entity/course.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateCourseDto } from '../dto/create-course.dto';
+import { Users2 } from 'src/modules/auth/entity/users2.entity';
 
 @Injectable()
-export class CourseService {}
+export class CourseService {
+  constructor(
+    @InjectRepository(Course)
+    private readonly courseRepository: Repository<Course>,
+    @InjectRepository(Users2)
+    private readonly userRepository: Repository<Users2>,
+  ) {}
+
+  async createCourse(createCourseDto: CreateCourseDto): Promise<Course> {
+    const course = this.courseRepository.create(createCourseDto);
+    return this.courseRepository.save(course);
+  }
+
+  async findAll(): Promise<Course[]> {
+    const courses = await this.courseRepository.find({});
+    return courses;
+  }
+
+  async findById(courseId: number): Promise<Course> {
+    const course = await this.courseRepository.findOne({
+      where: { id: courseId },
+    });
+    if (!course) {
+      throw new NotFoundException('Course not found');
+    }
+    return course;
+  }
+
+  async deleteCourseById(
+    courseId: number,
+  ): Promise<{ statusCode: number; message: string }> {
+    const result = await this.courseRepository.delete(courseId);
+    if (result.affected === 0) {
+      throw new NotFoundException('Course not found');
+    }
+    return {
+      statusCode: 200,
+      message: 'Course deleted successfully',
+    };
+  }
+
+  async joinCourseByUser(
+    userId: number,
+    courseId: number,
+  ): Promise<{ course: Course; message: string }> {
+    const user = await this.userRepository.findOne({
+      where: { id: userId },
+      relations: ['courses'],
+    });
+    const course = await this.courseRepository.findOneBy({ id: courseId });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    if (!course) {
+      throw new NotFoundException('Course not found');
+    }
+    // it is used to eensure user.courses is an array
+    user.courses ??= [];
+    // Check if the course is already joined by the current user
+    const alreadyJoined = user.courses.some((c) => c.id === course.id);
+    if (alreadyJoined) {
+      throw new BadRequestException('Course already joined by the user');
+    }
+    user.courses.push(course);
+    await this.userRepository.save(user);
+    return { message: 'Course joined successfully', course };
+  }
+
+  //find total learners for a single courser course
+  async findTotalLearnersInACourse(
+    courseId: number,
+  ): Promise<{ statusCode: number; totalLearners: number }> {
+    const [course, totalLearners] = await this.courseRepository.findAndCount({
+      where: { id: courseId },
+      relations: ['users'], // \ to load the users relation
+    });
+
+    if (!course) {
+      throw new NotFoundException('Course not found');
+    }
+    return {
+      statusCode: 200,
+      totalLearners: totalLearners ?? 0,
+    };
+  }
+
+  async fetchCourseOfUser(userId: number): Promise<{ data: Course[] }> {
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    const userWithCourses = await this.userRepository.findOne({
+      where: { id: userId },
+      relations: ['courses'],
+    });
+    if (!userWithCourses) {
+      throw new NotFoundException('User not found');
+    }
+    return { data: userWithCourses.courses || [] };
+  }
+}
