4ca9a91a6c8b98a70000aa5c5b23993f1cb111b1
sijalneupane <sijalneupane5@gmail.com>
2025-06-25 16:01:59 +0545

    Feat: ondelete:'CASCADE' set in many to many relation in users2 entity

--- src/modules/auth/entity/users2.entity.ts
+++ src/modules/auth/entity/users2.entity.ts
@@ -1,26 +1,29 @@
-import { Entity, JoinTable, ManyToMany } from "typeorm";
-import { PrimaryGeneratedColumn, Column } from "typeorm";
-import { Role } from "../enum/role.enum";
-import { Course } from "src/modules/course/entity/course.entity";
+import { Entity, JoinTable, ManyToMany } from 'typeorm';
+import { PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Role } from '../enum/role.enum';
+import { Course } from 'src/modules/course/entity/course.entity';
 
 @Entity()
 export class Users2 {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({ unique: true })
-    username: string;
+  @Column({ unique: true })
+  username: string;
 
-    @Column({ unique: true })
-    email: string;
+  @Column({ unique: true })
+  email: string;
 
-    @Column({ type: 'enum', enum: Role, default: Role.USER })
-    role: Role;
-    
-    @Column()
-    password: string;
-    
-    @ManyToMany(() => Course, (course) => course.users,{cascade: true})
-    @JoinTable({ name: 'users2_course' })
-    courses: Course[];
+  @Column({ type: 'enum', enum: Role, default: Role.USER })
+  role: Role;
+
+  @Column()
+  password: string;
+
+  @ManyToMany(() => Course, (course) => course.users, {
+    cascade: true,
+    onDelete: 'CASCADE',
+  })
+  @JoinTable({ name: 'users2_course' })
+  courses: Course[];
 }
